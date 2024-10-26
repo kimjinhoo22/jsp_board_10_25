8863693531b7fc39ce5abc07feea6281cb1cd25a
kimjinhoo <wlsgnwkd22@naver.com>
2024-10-26 16:26:46 +0900

    helloServlet 생성

---
+++ src/main/java/com/sbs/jsp/board/servlet/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.sbs.jsp.board.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+        resp.getWriter().append("hello~~2");
+    }
+}
